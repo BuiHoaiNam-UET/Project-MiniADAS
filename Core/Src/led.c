67e74dc0bac40045f685f893ee92d56b4fe4d4f1
hoanglong21i5 <hoanglong212005@gmail.com>
2026-05-29 22:27:18 +0700

    Update led.c

--- Core/Src/led.c
+++ Core/Src/led.c
@@ -4,20 +4,20 @@
 void LED_Init(void)
 {
     /* Enable Clock cho GPIOC */
-    RCC->AHB1ENR |= (1U << 2);
+    RCC->AHB1ENR |= (1 << 2);
 
     /* Cấu hình PC0 Mode: Output (01) */
-    LED_GPIO_PORT->MODER &= ~(3U << (LED_PIN * 2U));
-    LED_GPIO_PORT->MODER |=  (1U << (LED_PIN * 2U));
+    LED_GPIO_PORT->MODER &= ~(3 << (LED_PIN * 2));
+    LED_GPIO_PORT->MODER |=  (1 << (LED_PIN * 2));
 
     /* Output Type: Push-pull */
-    LED_GPIO_PORT->OTYPER &= ~(1U << LED_PIN);
+    LED_GPIO_PORT->OTYPER &= ~(1 << LED_PIN);
 
     /* Output Speed: Low */
-    LED_GPIO_PORT->OSPEEDR &= ~(3U << (LED_PIN * 2U));
+    LED_GPIO_PORT->OSPEEDR &= ~(3 << (LED_PIN * 2));
 
     /* No Pull-up/Pull-down */
-    LED_GPIO_PORT->PUPDR &= ~(3U << (LED_PIN * 2U));
+    LED_GPIO_PORT->PUPDR &= ~(3 << (LED_PIN * 2));
 
     /* Trạng thái mặc định: Tắt */
     LED_Off();
@@ -26,19 +26,19 @@ void LED_Init(void)
 /* Bật LED (Active-High) */
 void LED_On(void)
 {
-    LED_GPIO_PORT->ODR |= (1U << LED_PIN);
+    LED_GPIO_PORT->ODR |= (1 << LED_PIN);
 }
 
 /* Tắt LED */
 void LED_Off(void)
 {
-    LED_GPIO_PORT->ODR &= ~(1U << LED_PIN);
+    LED_GPIO_PORT->ODR &= ~(1 << LED_PIN);
 }
 
 /* Đảo trạng thái (Toggle) */
 void LED_Toggle(void)
 {
-    LED_GPIO_PORT->ODR ^= (1U << LED_PIN);
+    LED_GPIO_PORT->ODR ^= (1 << LED_PIN);
 }
 
 /* Điều khiển LED cảnh báo theo ngưỡng khoảng cách */
